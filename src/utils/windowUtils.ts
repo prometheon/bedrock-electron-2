d951dee0adcbc1dc75cc47f8ef4e57bde7a958ef
Oleksandr Mieliekh <meleh.alexandr@gmail.com>
2022-11-17 01:57:45 +0800

    [8e8aeb841472] Fix bottom app offset for Windows

--- src/utils/windowUtils.ts
+++ src/utils/windowUtils.ts
@@ -28,13 +28,14 @@ export function setViewTopBound(topOffset = 0, rightOffset = 0) {
   const win = getWindow();
   const view = getView();
   const widthOffset = os.platform() === 'win32' && win.isMaximized() ? 12 : 0;
+  const heightOffset = os.platform() === 'win32' && win.isMaximized() ? 12 : 0;
 
   const bounds = win.getBounds();
   view?.setBounds({
     x: 0,
     y: topOffset,
     width: bounds.width - rightOffset - widthOffset,
-    height: bounds.height - topOffset,
+    height: bounds.height - topOffset - heightOffset,
   });
 }
 
