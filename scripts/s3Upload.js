0dcd5cb22dcaa1d17eaf2f0b6641ba0c66e08288
Oleksandr Mieliekh <meleh.alexandr@gmail.com>
2023-02-08 02:39:10 +0800

    [-] Check bucket for existing files

--- scripts/s3Upload.js
+++ scripts/s3Upload.js
@@ -2,60 +2,85 @@ const fs = require('fs');
 const path = require('path');
 const S3 = require('aws-sdk/clients/s3');
 
-const s3 = new S3({
-  accessKeyId: process.env.AWS_ID_APP_DOWNLOADS_BUCKET,
-  secretAccessKey: process.env.AWS_SECRET_APP_DOWNLOADS_BUCKET,
-});
+async function s3Upload() {
+  const s3 = new S3({
+    accessKeyId: process.env.AWS_ID_APP_DOWNLOADS_BUCKET,
+    secretAccessKey: process.env.AWS_SECRET_APP_DOWNLOADS_BUCKET,
+  });
 
-const buildPath = path.join(process.cwd(), 'release', 'build');
+  const { Contents } = await s3
+    .listObjectsV2({
+      Bucket: 'bedrock-apps',
+      Delimiter: '/',
+      Prefix: 'BedrockApp/',
+    })
+    .promise();
 
-const targetFiles = fs
-  .readdirSync(buildPath)
-  .filter((filename) => filename.endsWith('.exe') || filename.endsWith('.dmg'))
-  .map((filename) => ({ filename, filepath: path.join(buildPath, filename) }));
+  const bucketContents = Contents.filter((file) => !file.Key.endsWith('/'))
+    .filter((file) => !file.Key.includes('-RC')) // exclude RC versions, allow to re-upload them
+    .map((file) => file.Key);
 
-Promise.all(
-  targetFiles.map(
-    ({ filename, filepath }) =>
-      new Promise((resolve, reject) => {
-        const fileStream = fs.createReadStream(filepath);
-        fileStream.on('error', (err) => {
-          // eslint-disable-next-line no-console
-          console.error(err);
-          reject(err);
-        });
+  console.log(`>>>> bucketContents: `, bucketContents);
 
-        const normalizedFilename = filename.replace(/\s/g, '-');
+  const buildPath = path.join(process.cwd(), 'release', 'build');
 
-        const params = {
-          Bucket: 'bedrock-apps',
-          Key: `BedrockApp/${normalizedFilename}`,
-          Body: fileStream,
-          ContentType: filename.endsWith('.exe')
-            ? 'application/x-msdownload'
-            : 'application/octet-stream',
-        };
+  const targetFiles = fs
+    .readdirSync(buildPath)
+    .filter(
+      (filename) => filename.endsWith('.exe') || filename.endsWith('.dmg')
+    )
+    .map((filename) => ({
+      filename,
+      filepath: path.join(buildPath, filename),
+    }));
 
-        s3.upload(params, (err, data) => {
-          if (err) {
+  await Promise.all(
+    targetFiles.map(
+      ({ filename, filepath }) =>
+        new Promise((resolve, reject) => {
+          const fileStream = fs.createReadStream(filepath);
+          fileStream.on('error', (err) => {
             // eslint-disable-next-line no-console
             console.error(err);
             reject(err);
-            return;
-          }
+          });
 
-          resolve(data);
-          // eslint-disable-next-line no-console
-          console.log(`File ${normalizedFilename} was uploaded successfully`);
-        });
-      })
+          const normalizedFilename = filename.replace(/\s/g, '-');
+
+          const params = {
+            Bucket: 'bedrock-apps',
+            Key: `BedrockApp/${normalizedFilename}`,
+            Body: fileStream,
+            ContentType: filename.endsWith('.exe')
+              ? 'application/x-msdownload'
+              : 'application/octet-stream',
+          };
+
+          s3.upload(params, (err, data) => {
+            if (err) {
+              // eslint-disable-next-line no-console
+              console.error(err);
+              reject(err);
+              return;
+            }
+
+            resolve(data);
+            // eslint-disable-next-line no-console
+            console.log(`File ${normalizedFilename} was uploaded successfully`);
+          });
+        })
+    )
   )
-)
-  .then(() => {
-    // eslint-disable-next-line no-console
-    console.log('\nAll files were uploaded successfully');
-    return true;
-  })
-  .catch((err) => {
-    throw err;
-  });
+    .then(() => {
+      // eslint-disable-next-line no-console
+      console.log('\nAll files were uploaded successfully');
+      return true;
+    })
+    .catch((err) => {
+      throw err;
+    });
+}
+
+s3Upload().catch((err) => {
+  throw err;
+});
